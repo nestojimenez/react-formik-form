4f455478a7b0e188dabf3547b3e4fb05bc68adfc
nestojimenez <aguiladescalza@gmail.com>
2023-03-03 12:00:10 -0800

    Using Formik

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
+import { useFormik } from "formik";
 import React from "react";
+import Card from "./components/Card";
+
 // TODO: import useFormik from formik library
 
 function App() {
   // TODO: add a const called formik assigned to useFormik()
+  const formik = useFormik({
+    initialValues: {
+      email:'',
+      password:''
+    },
+    onSubmit: values => {
+      console.log('form: ', values);
+    },
+    validate: values =>{
+      let errors = {};
+      if(!values.email) errors.email = 'Required'
+      if(!values.password) errors.password = 'Required'
+      return errors;
+    }
+  });
+
 
   return (
     <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
+      <form onSubmit={formik.handleSubmit}>
+        <div className='card' style={{width:'24rem'}}>
+          <img src="" alt="" />
+          <div className='card-body'>
+              <h4 className='card-title'>My Form</h4>
+
+              <div className="input-group mb-3 mt-4">
+                <input name="email" type="text" className="form-control" placeholder="Recipient's email" aria-label="Recipient's username" aria-describedby="basic-addon2" onChange={formik.handleChange} value={formik.values.email}/>
+                <span className="input-group-text" id="basic-addon2">@example.com</span>
+              </div>
+              {formik.errors.email ? <p className="text-start" style={{color: 'red'}}>{formik.errors.email}</p>: null}
+              
+              <div className="input-group mb-3 mt-4">
+                <input name="password" type="text" className="form-control" placeholder="Enter Password" aria-label="Recipient's username" aria-describedby="basic-addon2" onChange={formik.handleChange} value={formik.values.password}/>
+                
+              </div>
+              {formik.errors.password ? <p className="text-start" style={{color: 'red'}}>{formik.errors.password}</p>: null}
+
+              <button className="btn btn-primary mt-5" type="submit">Submit</button>
+
+          </div>
+        </div>
+      </form>      
     </div>
   );
 }
